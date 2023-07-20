e8faeaf51b6aec1e3d0bbff4e4840d950eae0d87
PhoenixOutlaw <harshyadavv615@gmail.com>
2023-07-20 17:49:10 +0530

    intialization

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HomeScreen from './screens/HomeScreen';
+
+function App() {
+  return (
+    <div className="App bg-gray-900 h-screen">
+     <HomeScreen/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Navbar from "../components/navbar";
+
+export default function HomeScreen() {
+  return (
+    <div>
+      <Navbar/>
+      
+    </div>
+  );
+}
